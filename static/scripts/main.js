48e8e8ccbd2bec20b857915ccee4e3ce37ee4de8
FlutterRaj <rajkar921@gmail.com>
2021-02-26 01:46:51 +0530

    added dark-more, ver 2.0.0

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -5,3 +5,22 @@ AOS.init({
 });
 
 // Add your javascript here
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Swith theme Dynamically
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    document.getElementById("avatar").classList.add("avatar_dark");
+    document.getElementById("icon").classList.remove("fa-sun");
+    document.getElementById("icon").classList.add("fa-moon");
+  } else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    document.getElementById("avatar").classList.remove("avatar_dark");
+    document.getElementById("icon").classList.remove("fa-moon");
+    document.getElementById("icon").classList.add("fa-sun");
+  }
+}
+
+// Event Lisetner
+toggleSwitch.addEventListener('change', switchTheme);
